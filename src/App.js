30257f8f9122bf678be6af8d61c76281f653260b
peterbeeshop <muyambangopeter@gmail.com>
2021-07-06 14:38:23 +0200

    final code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Index from "./cryptoApp/Index";
-// import Coin from './cryptoApp/Coin'
+import "./App.css";
+import Coin from "./cryptoApp/Coin";
 import styled from "styled-components";
-
 function App() {
   const [coins, setCoins] = useState([]);
-  const [textInput, setTextInput] = useState("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=60&page=1&sparkline=false"
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=false"
     )
       .then((res) => res.json())
       .then((data) => {
@@ -17,94 +16,54 @@ function App() {
       })
       .catch((err) => console.log(err));
   }, []);
-  const filteredCoins = coins.filter((coin) => {
-    return coin.name.toLowerCase().includes(textInput.toLowerCase());
-  });
-  console.log(filteredCoins);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <Div>
+    <div className="coin-app">
       <H1>Cryptocurrency Prices by Market Cap</H1>
       <H1> 60 coins listed</H1>
       <P>The global crypto market cap is $1.47 Trillion</P>
-      <Index
-        textInput={textInput}
-        setTextInput={setTextInput}
-        coins={coins}
-        setCoins={setCoins}
-      />
-      <UL>
-        <LI>LOGO</LI>
-        <LI>NAME</LI>
-        <LI>SYMBOL</LI>
-        <LI>PRICE</LI>
-        <LI>MARKET-CAP</LI>
-      </UL>
-      {coins.map((coin) => {
-        const { symbol, name, current_price, id, image, market_cap } = coin;
-        return (
-          <InnerDiv key={id}>
-            <img src={image} alt="logo" height="30px" />
-            <h2>{name}</h2>
-            <h2>{symbol}</h2>
-            <h2>${current_price}</h2>
-            <h2>${market_cap}</h2>
-          </InnerDiv>
-        );
-      })}
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input
+            className="coin-input"
+            type="text"
+            onChange={handleChange}
+            placeholder="Search"
+          />
+        </form>
+      </div>
       {filteredCoins.map((coin) => {
-        const { symbol, name, current_price, id, image, market_cap } = coin;
         return (
-          <>
-            <InnerDiv key={id}>
-              <img src={image} alt="logo" height="30px" />
-              <h2>{name}</h2>
-              <h2>{symbol}</h2>
-              <h2>${current_price}</h2>
-              <h2>${market_cap}</h2>
-            </InnerDiv>
-          </>
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
         );
       })}
-    </Div>
+    </div>
   );
 }
-
 //styled components
-const Div = styled.div`
-  background-color: #040714;
-`;
-const InnerDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 80%;
-  margin: 0 auto;
-  text-align: center;
-  align-items: center;
-  border-bottom: 2px solid #031052;
-`;
 const H1 = styled.h1`
   text-align: center;
 `;
 const P = styled.p`
   text-align: center;
-`;
-const UL = styled.ul`
-  display: flex;
-  justify-content: space-between;
-  padding-right: 15px;
-  align-items: center;
-  width: 85%;
-  margin: 0 auto;
-  border: 2px solid black;
-  background-color: rgb(110, 109, 109);
-  font-size: 0;
-  padding: 15px;
-  list-style: none;
-`;
-const LI = styled.li`
-  color: wheat;
-  font-size: 14px;
-  display: inline-block;
-  /* margin-bottom: 10px; */
+  margin: 10px;
 `;
 export default App;
